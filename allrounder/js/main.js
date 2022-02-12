3815e35acc07a517cb9950d7a31bbecc89b98fc5
MdYonos <99536512+MdYonos@users.noreply.github.com>
2022-02-12 17:13:29 +0600

    Add files via upload

---
+++ allrounder/js/main.js
@@ -0,0 +1,8 @@
+// bx slider
+  $(document).ready(function(){
+    $('.slider_area').bxSlider();
+    $('.right_align_area').meanmenu({
+      meanMenucontainer: '#mobile_menu',
+      menuScreenWidth: 768,
+    });
+});
